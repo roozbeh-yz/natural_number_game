4fd0edb1adb63c10d4ad4b3ea8b8876935bb4cf6
Roozbeh Yousefzadeh <roozbeh.yz@gmail.com>
2023-07-20 11:35:36 +0800

    Update level16.lean

--- src/game/world10/level16.lean
+++ src/game/world10/level16.lean
@@ -20,17 +20,26 @@ lemma lt_aux_two (a b : mynat) : succ a ≤ b → a ≤ b ∧ ¬ (b ≤ a) :=
 begin [nat_num_game]
   intro h,
   split,
-  { apply le_trans a (succ a) b,
-    exact le_succ_self a,
-    exact h,
-  },
-  intro nh,
-  apply ne_succ_self a,
-  apply le_antisymm a (succ a),
-  exact le_succ_self a,
-  exact le_trans (succ a) b a h nh,
-
-
+  have h2:= le_succ (succ a) b h,
+  apply le_of_succ_le_succ,
+  exact h2,
+  intro g,
+  apply not_succ_le_self a,
+  have h2 := le_trans (succ a) b a,
+  apply h2(h) g,
+  --alternative proof:
+  --have h2:= le_succ (succ a) b h,
+  --cases g with c gc,
+  --cases h with d hd,
+  --rw gc at hd,
+  --rw succ_eq_add_one at hd,
+  --simp at hd,
+  --symmetry at hd,
+  --have f:= eq_zero_of_add_right_eq_self hd,
+  --rw ← add_assoc at f,
+  --rw add_one_eq_succ at f,
+  --apply succ_ne_zero (c+d),
+  --exact f,
 
 
 end
