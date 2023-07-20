f8d6a5fdfc4ec2123a55e0bdb36a57dc782c1cbf
Roozbeh Yousefzadeh <roozbeh.yz@gmail.com>
2023-07-20 11:11:09 +0800

    Update level6.lean

--- src/game/world10/level6.lean
+++ src/game/world10/level6.lean
@@ -25,16 +25,18 @@ $\le$ is antisymmetric. In other words, if $a\le b$ and $b\le a$ then $a = b$.
 -/
 theorem le_antisymm (a b : mynat) (hab : a ≤ b) (hba : b ≤ a) : a = b :=
 begin [nat_num_game]
-  cases hab with c hc,
-  cases hba with d hd,
-  rw hc at hd,
-  rw add_assoc at hd,
-  symmetry at hd,
-  have h := eq_zero_of_add_right_eq_self hd,
-  have h2 := add_right_eq_zero h,
-  rw h2 at hc,
-  rw hc,
-  exact add_zero a,
+  cases hab with c,
+  cases hba with d,
+  rw hab_h at hba_h,
+  rw add_assoc at hba_h,
+  symmetry at hba_h,
+  have h := eq_zero_of_add_right_eq_self hba_h,
+  have g := add_right_eq_zero h,
+  symmetry,
+  rw hab_h,
+  rw g,
+  rw add_zero,
+  refl,
 
 
 end
